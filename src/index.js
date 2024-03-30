f1f0ceac49d5d849b44388cad3711f0363bbb3e0
ibrahimser29 <92850843+ibrahimser29@users.noreply.github.com>
2024-03-31 02:37:48 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+      fontFamily: {
+        display: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      extend: {
+        fontSize: {
+          14: '14px',
+        },
+        backgroundColor: {
+          'main-bg': '#FAFBFB',
+          'main-dark-bg': '#20232A',
+          'secondary-dark-bg': '#33373E',
+          'light-gray': '#F7F7F7',
+          'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        },
+        borderWidth: {
+          1: '1px',
+        },
+        borderColor: {
+          color: 'rgba(0, 0, 0, 0.1)',
+        },
+        width: {
+          400: '400px',
+          760: '760px',
+          780: '780px',
+          800: '800px',
+          1000: '1000px',
+          1200: '1200px',
+          1400: '1400px',
+        },
+        height: {
+          80: '80px',
+        },
+        minHeight: {
+          590: '590px',
+        },
+        backgroundImage: {
+          'hero-pattern':
+            "url('https://img.freepik.com/free-photo/businessperson-s-hand-with-graphs-transparent-glass-board_23-2147838605.jpg?t=st=1711233003~exp=1711236603~hmac=0d5a2e9dd2582aed115d2c67b56d1b769d6d80f59e1e285b8d9a2efbf639c4a6&w=1060')",
+        },
+      },
+    },
+    plugins: [],
+  };
